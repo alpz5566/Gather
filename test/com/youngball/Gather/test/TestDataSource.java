7e64aeb646f55004e408b1c48dc366093f6a1607
alpz5566 <451846271@qq.com>
2015-03-18 15:29:27 +0800

    框架搭建测试

---
+++ test/com/youngball/Gather/test/TestDataSource.java
@@ -0,0 +1,20 @@
+package com.youngball.Gather.test;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+
+public class TestDataSource {
+	
+	@Test
+	public void getConn() throws Exception{
+		ApplicationContext ac = new ClassPathXmlApplicationContext("beans.xml");
+		DataSource ds = (DataSource) ac.getBean("dataSource");
+		System.out.println(ds.getConnection());
+	}
+	
+	
+}
